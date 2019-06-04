050cd51ce8c379148e25e8157b59fa9ea9e779f5
LepekhovNikolay <gerb13@yandex.ru>
2019-06-05 00:19:56 +0300

    task3913 first forth parts

--- src/com/javarush/task/task39/task3913/LogParser.java
+++ src/com/javarush/task/task39/task3913/LogParser.java
@@ -1,6 +1,9 @@
 package com.javarush.task.task39.task3913;
 
+import com.javarush.task.task39.task3913.query.DateQuery;
+import com.javarush.task.task39.task3913.query.EventQuery;
 import com.javarush.task.task39.task3913.query.IPQuery;
+import com.javarush.task.task39.task3913.query.UserQuery;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -11,7 +14,7 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class LogParser implements IPQuery {
+public class LogParser implements IPQuery, UserQuery, DateQuery, EventQuery {
     private Path logDir;//директория с логами *.log
     private List<MyLog> logs = new ArrayList<>();
 
@@ -53,6 +56,316 @@ public class LogParser implements IPQuery {
         }
     }
 
+    //Start implementation of EventQuery
+
+    @Override
+    public int getNumberOfAllEvents(Date after, Date before) {
+        return getAllEvents(after, before).size();
+    }
+
+    @Override
+    public Set<Event> getAllEvents(Date after, Date before) {
+        Set<Event> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before,log.date))
+                set.add(log.event);
+        }
+        return set;
+    }
+
+    @Override
+    public Set<Event> getEventsForIP(String ip, Date after, Date before) {
+        Set<Event> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before,log.date) &&
+                    log.ip.equals(ip))
+                set.add(log.event);
+        }
+        return set;
+    }
+
+    @Override
+    public Set<Event> getEventsForUser(String user, Date after, Date before) {
+        Set<Event> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before,log.date) &&
+                    log.user.equals(user))
+                set.add(log.event);
+        }
+        return set;
+    }
+
+    @Override
+    public Set<Event> getFailedEvents(Date after, Date before) {
+        return getEventsWithStatus(after, before, Status.FAILED);
+    }
+
+    @Override
+    public Set<Event> getErrorEvents(Date after, Date before) {
+        return getEventsWithStatus(after, before, Status.ERROR);
+    }
+
+    private Set<Event> getEventsWithStatus(Date after, Date before, Status status) {
+        Set<Event> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before,log.date) &&
+                    log.status == status)
+                set.add(log.event);
+        }
+        return set;
+    }
+
+    @Override
+    public int getNumberOfAttemptToSolveTask(int task, Date after, Date before) {
+        int result = 0;
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.event == Event.SOLVE_TASK &&
+                    log.task == task)
+                result++;
+        }
+        return result;
+    }
+
+    @Override
+    public int getNumberOfSuccessfulAttemptToSolveTask(int task, Date after, Date before) {
+        int result = 0;
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.event == Event.DONE_TASK &&
+                    log.task == task /*&&
+                    log.status == Status.OK*/)
+                result++;
+        }
+        return result;
+    }
+
+    @Override
+    public Map<Integer, Integer> getAllSolvedTasksAndTheirNumber(Date after, Date before) {
+        return getAllAttempts(after, before, Event.SOLVE_TASK);
+    }
+
+    @Override
+    public Map<Integer, Integer> getAllDoneTasksAndTheirNumber(Date after, Date before) {
+        return getAllAttempts(after, before, Event.DONE_TASK);
+    }
+
+    private Map<Integer, Integer> getAllAttempts(Date after, Date before, Event event) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.event == event) {
+                Integer attempt = map.get(log.task);
+                if (attempt == null)
+                    attempt = 1;
+                else
+                    attempt++;
+                map.put(log.task, attempt);
+            }
+        }
+        return map;
+    }
+
+
+    //Start implementation of DateQuery
+
+    @Override
+    public Set<Date> getDatesForUserAndEvent(String user, Event event, Date after, Date before) {
+        Set<Date> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.user.equals(user) &&
+                    log.event == event)
+                set.add(log.date);
+        }
+        return set;
+    }
+
+    @Override
+    public Set<Date> getDatesWhenSomethingFailed(Date after, Date before) {
+        return getDatesWithStatus(after, before, Status.FAILED);
+    }
+
+    @Override
+    public Set<Date> getDatesWhenErrorHappened(Date after, Date before) {
+        return getDatesWithStatus(after, before, Status.ERROR);
+    }
+
+    private Set<Date> getDatesWithStatus(Date after, Date before, Status status) {
+        Set<Date> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.status == status)
+                set.add(log.date);
+        }
+        return set;
+    }
+
+    @Override
+    public Date getDateWhenUserLoggedFirstTime(String user, Date after, Date before) {
+        Set<Date> set = getDatesForUserAndEvent(user, Event.LOGIN, after, before);
+        if (set.size() == 0) {
+            return null;
+        } else {
+            Date result = new Date(Long.MAX_VALUE);
+            for (Date date : set) {
+                if (date.getTime() < result.getTime()) {
+                    result = date;
+                }
+            }
+            return result;
+        }
+    }
+
+    @Override
+    public Date getDateWhenUserSolvedTask(String user, int task, Date after, Date before) {
+        return getFirstDateTask(user, task, after, before, Event.SOLVE_TASK);
+    }
+
+    @Override
+    public Date getDateWhenUserDoneTask(String user, int task, Date after, Date before) {
+        return getFirstDateTask(user, task, after, before, Event.DONE_TASK);
+    }
+
+    private Date getFirstDateTask(String user, int task, Date after, Date before, Event event) {
+        Date date = null;
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.user.equals(user) &&
+                    log.event == event &&
+                    log.task == task) {
+                if (date == null)
+                    date = log.date;
+                else if (log.date.getTime() < date.getTime()){
+                    date = log.date;
+                }
+            }
+        }
+        return date;
+    }
+
+    @Override
+    public Set<Date> getDatesWhenUserWroteMessage(String user, Date after, Date before) {
+        return getDatesWithEvent(user, after, before, Event.WRITE_MESSAGE);
+    }
+
+    @Override
+    public Set<Date> getDatesWhenUserDownloadedPlugin(String user, Date after, Date before) {
+        return getDatesWithEvent(user, after, before, Event.DOWNLOAD_PLUGIN);
+    }
+
+    private Set<Date> getDatesWithEvent(String user, Date after, Date before, Event event) {
+        Set<Date> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.user.equals(user) &&
+                    log.event == event)
+                set.add(log.date);
+        }
+        return set;
+    }
+
+    //Start implementation of UserQuery
+    @Override
+    public Set<String> getAllUsers() {
+        Set<String> set = new HashSet<>();
+        for (MyLog log : logs) {
+            set.add(log.user);
+        }
+        return set;
+    }
+
+    @Override
+    public int getNumberOfUsers(Date after, Date before) {
+        Set<String> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date))
+                set.add(log.user);
+        }
+        return set.size();
+    }
+
+    @Override
+    public int getNumberOfUserEvents(String user, Date after, Date before) {
+        Set<Event> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.user.equals(user))
+                set.add(log.event);
+        }
+        return set.size();
+    }
+
+    @Override
+    public Set<String> getUsersForIP(String ip, Date after, Date before) {
+        Set<String> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.ip.equals(ip))
+                set.add(log.user);
+        }
+        return set;
+    }
+
+    @Override
+    public Set<String> getLoggedUsers(Date after, Date before) {
+        return getUsersByEvent(after, before, Event.LOGIN);
+    }
+
+    @Override
+    public Set<String> getDownloadedPluginUsers(Date after, Date before) {
+        return getUsersByEvent(after, before, Event.DOWNLOAD_PLUGIN);
+    }
+
+    @Override
+    public Set<String> getWroteMessageUsers(Date after, Date before) {
+        return getUsersByEvent(after, before, Event.WRITE_MESSAGE);
+    }
+
+    @Override
+    public Set<String> getSolvedTaskUsers(Date after, Date before) {
+        return getUsersByEvent(after, before, Event.SOLVE_TASK);
+    }
+
+    @Override
+    public Set<String> getSolvedTaskUsers(Date after, Date before, int task) {
+        return getUsersByEvent(after, before, Event.SOLVE_TASK, task);
+    }
+
+    @Override
+    public Set<String> getDoneTaskUsers(Date after, Date before) {
+        return getUsersByEvent(after, before, Event.DONE_TASK);
+    }
+
+    @Override
+    public Set<String> getDoneTaskUsers(Date after, Date before, int task) {
+        return getUsersByEvent(after, before, Event.DONE_TASK, task);
+    }
+
+    private Set<String> getUsersByEvent(Date after, Date before, Event event) {
+        Set<String> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.event == event/* &&
+                    log.status == Status.OK*/)
+                set.add(log.user);
+        }
+        return set;
+    }
+
+    private Set<String> getUsersByEvent(Date after, Date before, Event event, int task) {
+        Set<String> set = new HashSet<>();
+        for (MyLog log : logs) {
+            if (checkDate(after, before, log.date) &&
+                    log.event == event &&
+                    log.task == task /*&&
+                    log.status == Status.OK*/)
+                set.add(log.user);
+        }
+        return set;
+    }
+
+    //Start implementation IPQuery
     @Override
     public int getNumberOfUniqueIPs(Date after, Date before) {
         return getUniqueIPs(after, before).size();
@@ -62,8 +375,8 @@ public class LogParser implements IPQuery {
     public Set<String> getUniqueIPs(Date after, Date before) {
         Set<String> set = new HashSet<>();
         for (MyLog log : logs) {
-            if (checkDates(after, before, log.date))
-                set.add(log.ipString);
+            if (checkDate(after, before, log.date))
+                set.add(log.ip);
         }
         return set;
     }
@@ -72,8 +385,9 @@ public class LogParser implements IPQuery {
     public Set<String> getIPsForUser(String user, Date after, Date before) {
         Set<String> set = new HashSet<>();
         for (MyLog log : logs) {
-            if (log.user.equals(user) && checkDates(after, before, log.date))
-                set.add(log.ipString);
+            if (checkDate(after, before, log.date) &&
+                    log.user.equals(user))
+                set.add(log.ip);
         }
         return set;
     }
@@ -82,8 +396,9 @@ public class LogParser implements IPQuery {
     public Set<String> getIPsForEvent(Event event, Date after, Date before) {
         Set<String> set = new HashSet<>();
         for (MyLog log : logs) {
-            if (log.event == event && checkDates(after, before, log.date))
-                set.add(log.ipString);
+            if (checkDate(after, before, log.date) &&
+                    log.event == event)
+                set.add(log.ip);
         }
         return set;
     }
@@ -92,46 +407,40 @@ public class LogParser implements IPQuery {
     public Set<String> getIPsForStatus(Status status, Date after, Date before) {
         Set<String> set = new HashSet<>();
         for (MyLog log : logs) {
-            if (log.status == status && checkDates(after, before, log.date))
-                set.add(log.ipString);
+            if (checkDate(after, before, log.date) &&
+                    log.status == status)
+                set.add(log.ip);
         }
         return set;
     }
 
 
-    private boolean checkDates(Date after, Date before, Date current) {
+    private boolean checkDate(Date after, Date before, Date current) {
         if (after == null)
             after = new Date(0);
         if (before == null)
             before = new Date(Long.MAX_VALUE);
-        if (current.getTime() > after.getTime() && current.getTime() < before.getTime())
+        if (after.getTime() < current.getTime() &&
+                before.getTime() > current.getTime())
             return true;
         return false;
     }
 
     private class MyLog {
-        private int[] ip = new int[4];
-        private String ipString;
+        private String ip;
         private String user;
         private Date date;
         private Event event;
-        private int taskNumber;
+        private int task;
         private Status status;
 
         public MyLog(String ipString, String user, Date date, Event event, int taskNumber, Status status) {
-            ipParse(ipString);
             this.user = user;
             this.date = date;
             this.event = event;
-            this.taskNumber = taskNumber;
+            this.task = taskNumber;
             this.status = status;
-            this.ipString = ipString;
-        }
-        private void ipParse(String ipString) {
-            String[] ipParts = ipString.split("\\.");
-            for (int i = 0; i < 4; i++) {
-                ip[i] = Integer.parseInt(ipParts[i]);
-            }
+            this.ip = ipString;
         }
     }
 }
--- src/com/javarush/task/task39/task3913/Solution.java
+++ src/com/javarush/task/task39/task3913/Solution.java
@@ -7,10 +7,43 @@ public class Solution {
     public static void main(String[] args) {
 //        LogParser logParser = new LogParser(Paths.get("c:/logs/"));
         LogParser logParser = new LogParser(Paths.get("C:\\JavaRush\\JavaRushTasks\\4.JavaCollections\\src\\com\\javarush\\task\\task39\\task3913\\logs"));
+//        checkIPQuery(logParser);
+//        checkUserQuery(logParser);
+//        checkDateQuery(logParser);
+        checkEventQuery(logParser);
+    }
 
+    private static void checkIPQuery(LogParser logParser) {
         System.out.println(logParser.getNumberOfUniqueIPs(null, new Date()));
         System.out.println(logParser.getIPsForUser("Amigo", null, null));
         System.out.println(logParser.getIPsForEvent(Event.LOGIN, null, null));
         System.out.println(logParser.getIPsForStatus(Status.OK, null, new Date()));
+    }
+
+    private static void checkUserQuery(LogParser logParser) {
+        System.out.println("getAllUsers" + logParser.getAllUsers());
+        System.out.println("getNumberOfUsers all from now to infinity " + logParser.getNumberOfUsers(new Date(), null));
+        System.out.println("getNumberOfUserEvents for Amigo from now to infinity" + logParser.getNumberOfUserEvents("Amigo", new Date(), null));
+        System.out.println("getUsersForIP for 120.120.120.122 for all date" + logParser.getUsersForIP("120.120.120.122", null, null));
+        System.out.println("getLoggedUsers" + logParser.getLoggedUsers(null, null));
+        System.out.println("getDownloadedPluginUsers" + logParser.getDownloadedPluginUsers(null, null));
+        System.out.println("getWroteMessageUsers" + logParser.getWroteMessageUsers(null, null));
+        System.out.println("getSolvedTaskUsers" + logParser.getSolvedTaskUsers(null, null));
+        System.out.println("getSolvedTaskUsers task 15" + logParser.getSolvedTaskUsers(null, null, 15));
+    }
+
+    private static void checkDateQuery(LogParser logParser) {
+        System.out.println("1 getDatesForUserAndEvent for Eduard wrote messages: " + logParser.getDatesForUserAndEvent("Eduard Petrovich Morozko", Event.WRITE_MESSAGE, null, null));
+        System.out.println("2 getDatesWhenSomethingFailed: " + logParser.getDatesWhenSomethingFailed(null, null));
+        System.out.println("3 getDatesWhenErrorHappened: " + logParser.getDatesWhenErrorHappened(null, null));
+        System.out.println("4 getDateWhenUserLoggedFirstTime for Eduard: " + logParser.getDateWhenUserLoggedFirstTime("Eduard Petrovich Morozko", null, null));
+        System.out.println("5 getDateWhenUserSolvedTask for 1 task by Vasya: " + logParser.getDateWhenUserSolvedTask("Vasya Pupkin", 1, null, null));
+        System.out.println("6 getDateWhenUserDoneTask for task48 by Eduard" + logParser.getDateWhenUserDoneTask("Eduard Petrovich Morozko", 48, null, null));
+        System.out.println("7 getDatesWhenUserWroteMessage for Eduard" + logParser.getDatesWhenUserWroteMessage("Eduard Petrovich Morozko", null, null));
+        System.out.println("8 getDatesWhenUserDownloadedPlugin for Eduard" + logParser.getDatesWhenUserDownloadedPlugin("Eduard Petrovich Morozko", null, null));
+    }
+
+    private static void checkEventQuery(LogParser logParser) {
+        System.out.println("for task 2 succesful attemt is: " + logParser.getNumberOfSuccessfulAttemptToSolveTask(2, null, null));
     }
 }
